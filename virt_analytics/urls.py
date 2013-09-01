da2c0feeb6351d15d35f2d35871e0cbc1258bfdd
Yasan <devops.yasan@gmail.com>
2013-09-01 16:59:24 +0530

    start the project

---
+++ virt_analytics/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'virt_analytics.views.home', name='home'),
+    # url(r'^virt_analytics/', include('virt_analytics.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
